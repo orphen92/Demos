da4912801f136f1c89bd1b67ac392d5c82dd8688
DESKTOP-F9MS57I\sylva <orphen92300@gmail.com>
2021-02-10 00:25:21 +0100

    add some methods and var

--- css/ANIM_background-billes/js/functions.js
+++ css/ANIM_background-billes/js/functions.js
@@ -1,21 +1,37 @@
 addEventListener("DOMContentLoaded", (event) => {
-    console.log('Page est chargée');
+    let container = document.querySelector('.container');
     let index;
     let sprite; //20 * 20
     let width = window.innerWidth; //1920
     let height = window.innerHeight; //969
     
-    let iteration = Math.round((width /20) * (height/20) + 53); //works with 4704 4651.2
+    let iteration = Math.round((width /20) * (height/20) + 53); //works with 4704
 
 
 
-    for(index = 1; index <= iteration; index++) {
-        sprite = document.createElement('i');
-
-        document.querySelector('.container').appendChild(sprite);
+    function addSprite(){
+        for(index = 1; index <= iteration; index++) {
+            sprite = document.createElement('i');
+    
+            container.appendChild(sprite);
+        }
     }
 
+    function clearSprite(){
+        container.innerHTML = "";
+    }
+    
+    function resize() {
+        clearSprite();
+        width = window.innerWidth;
+        height = window.innerHeight; 
+        iteration = Math.round((width /20) * (height/20) + 53); //works with 4704
+        addSprite()
+      }
 
 
+      addSprite()
+
+      onresize = resize;
 
 });
